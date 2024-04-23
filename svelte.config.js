141958deb0e53f822e468e8495c847bc41c6e7f5
Patrick <81094295+TheRealMarine@users.noreply.github.com>
2024-04-23 12:36:48 +0200

    change configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/therealmarine.github.io';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
